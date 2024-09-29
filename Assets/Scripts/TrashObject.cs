9821f900c005e6314198cc57fed72f70a2a3659f
Akemisora <akemisora040503@gmail.com>
2024-09-29 11:36:37 +0700

    pick sampah

--- Assets/Scripts/TrashObject.cs
+++ Assets/Scripts/TrashObject.cs
@@ -4,19 +4,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Trash {
-    public class TrashObject : MonoBehaviour {
+    public class TrashObject : MonoBehaviour, IPickable {
 
+        [SerializeField] private Rigidbody2D body;
+
+        public void OnPick(Transform holdPoint) {
+            body.position = holdPoint.position;
+            body.transform.parent = holdPoint;
+            body.bodyType = RigidbodyType2D.Kinematic;
+        }
+
+        public void OnThrow(Vector2 velocity) {
+            body.transform.parent = null;
+            body.bodyType = RigidbodyType2D.Dynamic;
+            body.velocity = velocity;
+        }
 
         private void OnTriggerEnter2D(Collider2D other) {
             Debug.Log("Hello");    
         }
 
-    }
-
-
-    public interface IPickable {
-        void OnPick();
-        void OnThrow(Vector2 velocity);
     }
 
 }
