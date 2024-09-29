97f26c6ba15c9ff64e4aa8ff61c85ed996d1a25a
Akemisora <akemisora040503@gmail.com>
2024-09-29 17:29:26 +0700

    full trashcan

--- Assets/Scripts/TrashCan.cs
+++ Assets/Scripts/TrashCan.cs
@@ -7,11 +7,34 @@ namespace Trash {
         [SerializeField] private int maxCapacity = 10;
         [SerializeField] private int currentFilled;
 
+        [SerializeField] private SpriteRenderer spriteRenderer;
+        [SerializeField] private Sprite emptySprite;
+        [SerializeField] private Sprite fullSprite;   
+
+        [SerializeField] private Collider2D fillableCollider;
+        [SerializeField] private Collider2D fullCollider;
+
         public event Action<int, int> OnFillCountChanged = delegate { };
 
         public void OnInsert() {
             currentFilled++;
+            if (currentFilled == maxCapacity) {
+                Full();
+            }
             OnFillCountChanged(currentFilled, maxCapacity);
+
+            void Full() {
+                fillableCollider.enabled = false;
+                fullCollider.enabled = true;
+                spriteRenderer.sprite = fullSprite;
+            }
+        }
+
+        public void Empty() {
+            currentFilled = 0;
+            fillableCollider.enabled = true;
+            fullCollider.enabled = false;
+            spriteRenderer.sprite = emptySprite;
         }
 
     }
