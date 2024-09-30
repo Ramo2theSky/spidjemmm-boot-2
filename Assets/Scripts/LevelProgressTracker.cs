f0cca5bd9e2f2bc047e9dbe69a06295026c4365e
daistic <matotsuper@gmail.com>
2024-09-30 21:32:44 +0700

    scene level

--- Assets/Scripts/LevelProgressTracker.cs
+++ Assets/Scripts/LevelProgressTracker.cs
@@ -7,18 +7,15 @@ namespace Trash {
     public class LevelProgressTracker : MonoBehaviour {
 
         [SerializeField] private GameTimer timer;
-        [SerializeField] private int totalTrash = 5;
+        [SerializeField] private int totalTrash = 6;
         [SerializeField] private int trashCollected = 0;
 
         [SerializeField] private TMP_Text text;
-        [SerializeField] private GameState resultState;
 
         public static LevelProgressTracker Instance { get; private set; }
 
         private void Awake() {
             Instance = this;
-            totalTrash = FindObjectsByType<TrashObject>(FindObjectsSortMode.None).Length;
-            text.SetText($"{trashCollected}/{totalTrash}");
         }
 
         public void OnTrashDumped() {
@@ -35,12 +32,12 @@ namespace Trash {
         }
 
         private IEnumerator WinCoroutine() {
-            yield return new WaitForSeconds(1f);
-            OpenLeaderBoard();
+            yield return new WaitForSeconds(0.5f);
+            OpenLeaderBoard(timer);
         }
 
-        private void OpenLeaderBoard() {
-            SceneSelector.Instance.ChangeState(resultState);
+        private void OpenLeaderBoard(GameTimer timer) {
+            //throw new NotImplementedException();
         }
     }
 
